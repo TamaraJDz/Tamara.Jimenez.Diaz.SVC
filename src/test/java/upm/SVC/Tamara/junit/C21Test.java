a3b29dd81ea845dbb66ee353a0e14e59becb8fe4
Tamara <tamara_uky@hotmail.com>
2016-10-02 02:10:08 +0200

    Test C21 creado

---
+++ src/test/java/upm/SVC/Tamara/junit/C21Test.java
@@ -0,0 +1,23 @@
+package upm.SVC.Tamara.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import upm.SVC.Tamara.C21;
+
+public class C21Test {
+
+	private C21 c21;
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", c21.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c21.m2());
+	}
+
+}
