5f2db67144de6c40fd324e8730a1a438840b6873
Tamara <tamara_uky@hotmail.com>
2016-10-02 02:21:37 +0200

    Inicilización en los test

--- src/test/java/upm/SVC/Tamara/junit/C21Test.java
+++ src/test/java/upm/SVC/Tamara/junit/C21Test.java
@@ -2,6 +2,7 @@ package upm.SVC.Tamara.junit;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import upm.SVC.Tamara.C21;
@@ -9,6 +10,11 @@ import upm.SVC.Tamara.C21;
 public class C21Test {
 
 	private C21 c21;
+	
+    @Before
+    public void setUp() {
+        c21 = new C21();
+    }
 	
 	@Test
 	public void testM1() {
