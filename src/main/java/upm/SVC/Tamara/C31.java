52e64b1c2fbe8258a1bc1409f51d1e4bb50bd42f
Tamara <tamara_uky@hotmail.com>
2016-10-02 02:29:51 +0200

    Clase C31 creada

---
+++ src/main/java/upm/SVC/Tamara/C31.java
@@ -0,0 +1,14 @@
+package upm.SVC.Tamara;
+
+public class C31 {
+	
+	public String m1(){
+		String name = "m1";
+		return name;
+	}
+	
+	public String m2(){
+		String name = "m2";
+		return name;
+	}
+}
